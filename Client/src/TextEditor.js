b8e52b2c1e7d0f0d82bd860223b9a0f55393d704
“ahmedgamal171” <ahmed682000@gmail.com>
2022-05-15 02:50:50 +0200

    Adding style for client

--- Client/src/TextEditor.js
+++ Client/src/TextEditor.js
@@ -2,16 +2,27 @@ import { useCallback } from 'react'
 import "quill/react/quill.snow.css"
 import Quill from 'quill'
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
 export default function TextEditor() { 
   const WrapperRef = useCallback(wrapper => {
 
    if(wrapper==null) return 
-   
+
    wrapper.innerHTML=""
    const editor = document.createElement('div')
    wrapper.append(editor)
-    new Quill(editor ,{theme:'snow'})
+    new Quill(editor ,{theme:'snow',modules:{toolbar:TOOLBAR_OPTIONS}})
   },[])
 
-  return <div id="container" ref={WrappeRef}></div>
+  return <div className="container" ref={WrappeRef}></div>
 }
