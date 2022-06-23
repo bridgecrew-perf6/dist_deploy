e996c4bb1039993237e101d71075bf846abd9378
marwanelmessiry <marwanelmessiry2000@gmail.com>
2022-06-23 23:34:44 +0200

    deploy client

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,7 +6,26 @@ import {
   Redirect,
 } from "react-router-dom"
 import { v4 as uuidV4 } from 'uuid'
-
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV=="production") 
+{
+ app.use(express.static('build'));
+ app.get('*',(req,res)=>
+ {
+  req.sendFile(path.resolve(__dirname,'build','index.html'));
+ }) 
+}
+app.listen(port,(err)=>
+{
+  if (err) {
+    return console.log(err);
+    console.log ('server running on port: ',port);
+    
+  }
+} )
 function App() {
   return (
     <Router>
